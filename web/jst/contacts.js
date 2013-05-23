3d5762a9f36741530cb462db8df91bdcaa8c2376
kevin olson <acidjazz@gmail.com>
2013-05-23 02:53:22 +0000

    search. phew.

--- web/jst/contacts.js
+++ web/jst/contacts.js
@@ -8,6 +8,60 @@ var contacts = {
 
   handlers: function() {
     $('.contacts .contact').dblclick(contacts.select);
+    $('.contacts .search-button').click(contacts.search.toggle);
+    $('.contacts .search .close').click(contacts.search.close);
+    $('.contacts .search input').keyup(contacts.search.keyup);
+  },
+
+  search: {
+
+    toggle: function() {
+      $('.contacts .search, .contacts .search-button').toggleClass('on');
+    },
+
+    keyup: function(e) {
+
+      var val = $(this).val();
+      var reg = new RegExp(val, 'ig');
+
+      contacts.search.reset();
+
+      if (val == '') {
+        $('.search .close').removeClass('on');
+        return true;
+      }
+      $('.search .close').addClass('on');
+
+      $('.contact').each(function(i, obj) {
+
+        var ndiv = $(obj).find('.name');
+
+        if (!$(obj).data('name').match(reg)) {
+          $(obj).hide();
+        } else {
+          ndiv.html(ndiv.html().replace(reg, '<span>' + val + '</span>'));
+        }
+      });
+
+    },
+
+    reset: function() {
+
+      // reset .. show them all and remove the highlights
+      $('.contact').show().each(function(i, obj) {
+        $(obj).find('.name').html($(obj).data('name'));
+      });
+
+    },
+
+    close: function() {
+
+      $('.contacts .search input').val('');
+      $('.contacts .search, .contacts .search-button').removeClass('on');
+      contacts.search.reset();
+
+    }
+
   },
 
   import: function() {
